30080dfceed2e811dff29302fe9149a586f4abea
MedveMarci <martonmende@gmail.com>
2026-05-04 16:07:21 +0200

    Added config for translations

---
+++ RadioMenuAPI/Config.cs
@@ -0,0 +1,8 @@
+﻿namespace RadioMenuAPI;
+
+public class Config
+{
+    public string DisabledLabel { get; set; } = " [disabled]";
+    public string FooterSelectHint { get; set; } = "Range = Next | Toggle = Select";
+    public string FooterUnlockHint { get; set; } = "Range = Next | Toggle = Unlock";
+}

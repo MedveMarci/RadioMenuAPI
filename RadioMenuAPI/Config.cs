a3651071928c6f2eaa4f6a01cc5fe054c0f29fbc
MedveMarci <martonmende@gmail.com>
2026-05-08 14:03:02 +0200

    Redesigned PagerMenu

--- RadioMenuAPI/Config.cs
+++ RadioMenuAPI/Config.cs
@@ -4,6 +4,6 @@ public class Config
 {
     public bool Debug { get; set; } = false;
     public string DisabledLabel { get; set; } = " [disabled]";
-    public string FooterSelectHint { get; set; } = "Range = Next | Toggle = Select";
-    public string FooterUnlockHint { get; set; } = "Range = Next | Toggle = Unlock";
+    public string FooterSelectHint { get; set; } = "Range: next  ·  Toggle: select";
+    public string FooterUnlockHint { get; set; } = "Range: next  ·  Toggle: unlock";
 }
